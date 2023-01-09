72bc6b40b2960c6f8b60f6a373f36e7eb60d92d6
Rafael Souza <souza_rafael.97@outlook.com>
2023-01-09 01:55:29 -0300

    feat: Adiciona Certificado de Inglês

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsChevronDoubleDown } from 'react-icons/bs';
-import { MdLocationPin } from 'react-icons/md'
+import { MdLocationPin } from 'react-icons/md';
 import { Link } from "react-scroll";
+import { FaFileDownload } from "react-icons/fa";
+import certificate from '../assets/hero/EF_SET_Certificate.jpg';
 
 const About = () => {
+  const [showImage, setImage] = useState(false);
+
+  function handleClick() {
+    setImage(true);
+  }
+
+  function handleClose() {
+    setImage(false);
+  }
+
   return (
     <div
       name="sobre mim"
@@ -27,7 +39,32 @@ const About = () => {
         </p>
           
         <p className="text-xl mt-7 text-justify">
-          Além disso, tenho cerca de 5 anos de experiência trabalhando em manutenção e operação em empresas multinacionais de grande porte, o que me deu uma base sólida em resolução de problemas e trabalho em equipe. No momento, estou em transição de carreira, buscando conhecimento e oportunidades no mundo do desenvolvimento web. Estou sempre estudando, buscando novas formas de aprender, e estou interessado em encontrar uma oportunidade de colocar minhas habilidades em prática.
+          Além disso, tenho cerca de 5 anos de experiência trabalhando em manutenção e operação em empresas multinacionais de grande porte, o que me deu uma base sólida em resolução de problemas e trabalho em equipe. Tenho inglês avançado, com <span onClick={handleClick} className="hover:underline cursor-pointer">proficiência no nível <span className="font-helvetica">C1</span></span>. No momento, estou em transição de carreira, buscando conhecimento e oportunidades no mundo do desenvolvimento web. Estou sempre estudando, buscando novas formas de aprender, e estou interessado em encontrar uma oportunidade de colocar minhas habilidades em prática.
+            {showImage && (
+              <div className="fixed top-0 bottom-0 left-0 right-0 md:flex items-center justify-center bg-black bg-opacity-75 z-50 overflow-scroll" style={{height: "100vh", width: "100vw"}}>
+                <div className="flex flex-col justify-center">
+                  <button onClick={handleClose} className="fixed top-0 right-0 btn bg-red-500 text-white rounded-full px-4 py-2 m-4">
+                    X
+                  </button>
+
+                  <a
+                    href="/EF_SET_Certificate.pdf"
+                    className="fixed top-5 right-24 text-blue-500"
+                    download={true}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <button>
+                      <>
+                        <FaFileDownload size={32} />
+                      </>
+                    </button>
+                    </a>
+
+                </div>
+                <img src={ certificate } alt="English Certificate" className="w-max sm:w-6/6 md:w-5/6 lg:max-w-2xl" />
+              </div>
+            )}
         </p>
 
         <p className="flex justify-center text-xl tech text center items-center text-[1.23rem] mt-20 text-gray-300">
